896d17593dfb77adbe82ede67a5cb293a3bcfd9d
Rusev11 <ivaylo.rusev@outlook.com>
2021-01-25 22:46:38 +0200

    First problem solved

--- Java Fundamentals/Basic Syntax, Conditional Statements and Loops/src/LAB/StudentInformation.java
+++ Java Fundamentals/Basic Syntax, Conditional Statements and Loops/src/LAB/StudentInformation.java
@@ -1,7 +1,13 @@
 package LAB;
 
+import java.util.Scanner;
+
 public class StudentInformation {
     public static void main(String[] args) {
-        System.out.println("adawd");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
     }
 }
