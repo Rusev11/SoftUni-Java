672287b1721183eea305fe2d0dd02f7aff2bd217
Ivaylo Rusev <ivaylo.rusev@outlook.com>
2021-01-27 15:46:09 +0200

    Problem 4 solved

---
+++ Java Fundamentals/Data Types and Variables/src/LAB/TownInfo.java
@@ -0,0 +1,13 @@
+package LAB;
+
+import java.util.Scanner;
+
+public class TownInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String town = scanner.nextLine();
+        String population = scanner.nextLine();
+        String area = scanner.nextLine();
+        System.out.printf("Town %s has population of %s and area %s square km.", town, population, area);
+    }
+}
