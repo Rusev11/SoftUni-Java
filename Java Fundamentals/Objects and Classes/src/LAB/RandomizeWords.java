33577231737f5fa2c6a1a5405e2699f411fad50d
Rusev11 <ivaylo.rusev@outlook.com>
2021-01-30 22:41:13 +0200

    Problem 1 solved

---
+++ Java Fundamentals/Objects and Classes/src/LAB/RandomizeWords.java
@@ -0,0 +1,21 @@
+package LAB;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomizeWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+        Random random = new Random();
+        for (int i = 0; i < words.length; i++) {
+            int randomIndex = random.nextInt(words.length);
+            String currentWord = words[i];
+            words[i] = words[randomIndex];
+            words[randomIndex] = currentWord;
+        }
+        for (String word: words){
+            System.out.println(word);
+        }
+    }
+}
