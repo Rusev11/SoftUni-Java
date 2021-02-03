b15b1db7732b7ce8038eb9df76208bdb5efcfb1f
Ivaylo Rusev <ivaylo.rusev@outlook.com>
2021-02-04 00:11:42 +0200

    LAB Problem 4 solved

--- Java Fundamentals/Associative Arrays/src/LAB/WordFilter.java
+++ Java Fundamentals/Associative Arrays/src/LAB/WordFilter.java
@@ -10,10 +10,10 @@ import java.util.stream.Stream;
 public class WordFilter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> words = Arrays.stream(scanner.nextLine().split(" "))
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
                 .filter(w -> w.length() % 2 == 0)
-                .collect(Collectors.toList());
+                .toArray(String[]::new);
         System.out.println(String.join(System.lineSeparator(),words));
     }
-    
+
 }
