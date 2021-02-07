2217179681552f838adf5fcc1288bfdf6d9e45cb
Rusev11 <ivaylo.rusev@outlook.com>
2021-02-07 21:52:13 +0200

    LAB Problem 3 solved

---
+++ Java Fundamentals/Text Processing/src/LAB/Substring.java
@@ -0,0 +1,18 @@
+package LAB;
+
+import java.util.Scanner;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String input = scanner.nextLine();
+        String result = String.join("",input.split(word));
+
+        while (input.length() != result.length()) {
+            input=result;
+            result = String.join("",input.split(word));
+        }
+        System.out.println(String.join("", result));
+    }
+}
