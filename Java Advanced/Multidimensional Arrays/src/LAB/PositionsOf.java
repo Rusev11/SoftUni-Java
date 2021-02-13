3e0e60be1b53bedc1b9cae760e9f4eaa520e6398
Rusev11 <ivaylo.rusev@outlook.com>
2021-02-13 14:04:06 +0200

    Multidimensional Arrays LAB Problem 2 solved

---
+++ Java Advanced/Multidimensional Arrays/src/LAB/PositionsOf.java
@@ -0,0 +1,33 @@
+package LAB;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PositionsOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] line = scanner.nextLine().split(" ");
+        int rows = Integer.parseInt(line[0]);
+        int cols = Integer.parseInt(line[1]);
+        ArrayList<String[]> lines = new ArrayList<>();
+        boolean found = false;
+
+        for (int row = 0; row < rows; row++) {
+            lines.add(scanner.nextLine().split(" "));
+        }
+
+        String number = scanner.nextLine();
+        
+        for (int row = 0; row < lines.size(); row++) {
+            for (int col = 0; col < lines.get(row).length; col++) {
+                if (lines.get(row)[col].equals(number)) {
+                    System.out.println(row + " " + col);
+                    found = true;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("not found");
+        }
+    }
+}
